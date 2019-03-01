f8bf802312f8ebfc9e42a990f4469f70bdea5a86
onnoo <sweyjw@gmail.com>
2019-03-02 00:40:10 +0900

    2019.03.02 Day 60

---
+++ Baekjoon/15657.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int *arr, *include;
+
+void func(int idx)
+{
+    if (idx == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << arr[include[i]] << ' ';
+        cout << '\n';
+    }
+    else
+    {
+        for (int i = include[idx]; i < N; i++)
+        {
+            include[idx] = i;
+            if (idx != M - 1)
+                include[idx + 1] = i;
+            func(idx + 1);
+        }
+    }
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    arr = (int *)malloc(sizeof(int) * N);
+    include = (int *)calloc(M, sizeof(int));
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    sort(arr, arr + N);
+    func(0);
+
+    return 0;
+}
