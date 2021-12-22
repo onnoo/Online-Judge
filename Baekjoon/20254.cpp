368dca9a781a67f91a33f338e8c7b22302dd2151
onnoo <sweyjw@gmail.com>
2021-12-22 22:34:15 +0900

    2021.12.22 Solve 1 problem!

---
+++ Baekjoon/20254.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << 56 * a + 24 * b + 14 * c + 6 * d << '\n';
+    return 0;
+}
