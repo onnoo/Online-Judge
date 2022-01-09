4fddd15f10de95678d56f9df63f6a679f57e2ffb
onnoo <sweyjw@gmail.com>
2022-01-09 22:29:36 +0900

    2022.01.09 Solve 2 problems!

---
+++ Baekjoon/24078.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int X;
+    cin >> X;
+
+    cout << X % 21 << '\n';
+    return 0;
+}
---
+++ Baekjoon/24082.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    long long X;
+    cin >> X;
+
+    cout << X * X * X << '\n';
+    return 0;
+}
